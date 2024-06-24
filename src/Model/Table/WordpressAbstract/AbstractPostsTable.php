333659500f310a5b1e23840cf9ed460ccea2f921
Webmaster <root@debian.localhost>
2024-06-24 17:09:49 +0000

    Implement findPosts() and findPublishedPosts()

--- src/Model/Table/WordpressAbstract/AbstractPostsTable.php
+++ src/Model/Table/WordpressAbstract/AbstractPostsTable.php
@@ -7,6 +7,24 @@ use Cake\ORM\Table;
 abstract class AbstractPostsTable extends Table
 {
 
+    public function findPosts($type = 'all', $options = [])
+    {
+        $defaults = [
+            'conditions' => [
+                'post_type' => 'post',
+            ],
+            'order' => 'post_date DESC',
+            'limit' => 5,
+        ];
+        $options += $defaults;
+        return $this->find($type, $options);
+    }
 
+    public function findPublishedPosts($type = 'all', $options = [])
+    {
+        $options['conditions']['post_type'] = 'post';
+        $options['conditions']['post_status'] = 'publish';
+        return $this->findPosts($type, $options);
+    }
 
 }

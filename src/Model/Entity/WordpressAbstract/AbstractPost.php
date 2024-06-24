f9869659231c8383570a4782d7ed54555f071c57
Webmaster <root@debian.localhost>
2024-06-24 17:11:55 +0000

    Start matching post URL to WP permalink

--- src/Model/Entity/WordpressAbstract/AbstractPost.php
+++ src/Model/Entity/WordpressAbstract/AbstractPost.php
@@ -7,6 +7,37 @@ use Cake\ORM\Entity;
 abstract class AbstractPost extends Entity
 {
 
-
+    public function _getUrl()
+    {
+        $permalink_structure = \Cake\Core\Configure::read(
+            'CakePHPWordpress.Options.permalink_structure'
+        );
+        $placeholders = [
+            '%year%',
+            '%monthnum%',
+            '%day%',
+            '%hour%',
+            '%minute%',
+            '%second%',
+            '%post_id%',
+            '%postname%',
+            '%category%',
+            '%author%',
+        ];
+        $values = [
+            $this->post_date->format('Y'),
+            $this->post_date->format('m'),
+            $this->post_date->format('d'),
+            $this->post_date->format('H'),
+            $this->post_date->format('i'),
+            $this->post_date->format('s'),
+            $this->ID,
+            $this->post_name,
+            'categ',
+            $this->post_author,
+        ];
+        $url = str_replace($placeholders, $values, $permalink_structure);
+        return \Cake\Routing\Router::url($url);
+    }
 
 }

34ef684bd76075d76534dab22cc108a4a0045f79
Webmaster <root@debian.localhost>
2024-06-24 11:05:53 +0000

    Use first configured blog as default

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,3 +2,8 @@
 
 // Load configuration from CakePHPWordpress.php located in APP/config
 \Cake\Core\Configure::load($this->getName(), 'default');
+
+// Pick the first configured blog to be the default blog
+$blogList = \Cake\Core\Configure::read('CakePHPWordpress.blogList');
+$blogSymbol = array_keys($blogList)[0];
+\Cake\Core\Configure::write('CakePHPWordpress.defaultBlog', $blogSymbol);

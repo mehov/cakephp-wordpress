793c47c685d0dd3c66708c6b09e140417c42f56a
mehov <mehov@users.noreply.github.com>
2024-11-13 12:54:33 +0000

    Perform checks before accessing post_tags and categories

--- templates/Posts/view_post.php
+++ templates/Posts/view_post.php
@@ -1,25 +1,30 @@
 <?php
 $this->assign('title', $post->post_title);
 $this->Html->meta('description', $post->post_excerpt, ['block' => true]);
-$this->Html->meta(
-    'keywords',
-    implode(',', \Cake\Utility\Hash::extract($post->post_tags, '{n}.term.name')),
-    ['block' => true]
-);
+if (is_array($post->post_tags)) {
+    $this->Html->meta(
+        'keywords',
+        implode(',', \Cake\Utility\Hash::extract($post->post_tags, '{n}.term.name')),
+        ['block' => true]
+    );
+}
 ?>
 <article itemscope itemtype="https://schema.org/Article" itemid="<?= $post->guid?>">
     <h1 itemprop="name headline"><?= $post->post_title?></h1>
     <p>
         <a href="<?= $post->url?>" itemprop="url"><time itemprop="dateModified" datetime="<?= $post->post_modified->toIso8601String()?>"><?= $post->post_modified->toFormattedDateString()?></time></a>
+<?php if (is_array($post->categories)): ?>
         in
 <?php foreach ($post->categories as $category): ?>
         <a href="<?= $category->url?>"><b><?= $category->term->name?></b></a>
 <?php endforeach; ?>
+<?php endif; ?>
     </p>
     <hr>
     <div itemprop="articleBody">
 <?= $post->the_content()?>
     </div>
+<?php if (is_array($post->post_tags)): ?>
     <div>
 <?php foreach($post->post_tags as $post_tag): ?>
         <a href="<?= $post_tag->url?>" class="button button-outline">
@@ -27,4 +32,5 @@ $this->Html->meta(
         </a>
 <?php endforeach; ?>
     </div>
+<?php endif; ?>
 </article>

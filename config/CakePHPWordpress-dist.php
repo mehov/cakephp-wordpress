749b1eed08770d23c08fedd281d8004fea9c585d
mehov <mehov@users.noreply.github.com>
2024-11-14 18:25:56 +0000

    Remove extra spaces

--- config/CakePHPWordpress-dist.php
+++ config/CakePHPWordpress-dist.php
@@ -7,12 +7,12 @@ return [
         'blogList' => [
             'ABC' => [ // Arbitrary UPPERCASE key identifying this configuration
                 'name' => 'ABC Blog', // Display name for this blog
-                 // Datasource name from APP/config/app_local.php
+                // Datasource name from APP/config/app_local.php
                 'datasource' => 'wordpress', // used to connect to database
                 // Some installs, particularly multisite networks, prefix tables
                 'tablePrefix' => null, // example values: wp_, wp_2_, wp_3_ etc.
                 'type' => 'Wordpress6', // currently Wordpress6
-                 // Local overrides expected in APP/src/Model/(Entity|Table)/$localPath
+                // Local overrides expected in APP/src/Model/(Entity|Table)/$localPath
                 'localPath' => 'CakePHPWordpress', // can be any value
             ],
         ],

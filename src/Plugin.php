2f06cff5eb0f0f4cae15fb0118dddced7a790359
mehov <mehov@users.noreply.github.com>
2024-11-18 18:22:39 +0000

    Remove unnecessary bootstrap() declaration

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,14 +3,8 @@
 namespace CakePHPWordpress;
 
 use Cake\Core\BasePlugin;
-use Cake\Core\PluginApplicationInterface;
 
 class Plugin extends BasePlugin
 {
-
-    public function bootstrap(PluginApplicationInterface $app): void
-    {
-        parent::bootstrap($app);
-    }
 
 }
